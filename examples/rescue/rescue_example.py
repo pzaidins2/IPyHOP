d8b6086f6b6556f5fdf9ad493a934ad91237878d
pzaidins2 <paulzaidins2@gmail.com>
2022-06-20 10:19:30 -0400

    added replan to rescue example

--- examples/rescue/rescue_example.py
+++ examples/rescue/rescue_example.py
@@ -9,7 +9,7 @@ from examples.rescue.domain.rescue_methods import methods
 from examples.rescue.domain.rescue_actions import actions
 from examples.rescue.problem.rescue_problem_1 import init_state, task_list_1, task_list_2
 from ipyhop import IPyHOP, MonteCarloExecutor, planar_plot
-
+# from ipyhop.failure_handler import post_failure_tasks
 
 # ******************************************        Main Program Start      ****************************************** #
 def main():
@@ -20,7 +20,7 @@ def main():
     plan = planner.plan(init_state, task_list_2, verbose=0)
     graph = planner.sol_tree
 
-    planar_plot(graph, root_node=0)
+    # planar_plot(graph, root_node=0)
 
     print('Plan: ')
     for action in plan:
@@ -33,6 +33,16 @@ def main():
     ed['stat'].append((len(planner.sol_tree.nodes), len(plan), planner.iterations))
     print(ed)
     print(ed['exec'][-1][-1][1])
+
+    # ('a_move_euclidean', 'r1', (1, 1), (2, 2), None)
+    fail_node = ('a_move_euclidean', 'r1', (1, 1), (2, 2), None)
+    fail_index = plan.index( fail_node )
+    planner.sol_plan = plan[ :fail_index ]
+    state_list = planner.simulate( init_state )
+    new_task_list = planner.replan( state_list[-1], fail_node, verbose=3 )
+    print(new_task_list)
+    print(planner.iterations)
+    planar_plot(graph,0)
 
 # ******************************************        Main Program End        ****************************************** #
 # ******************************************    Demo / Test Routine         ****************************************** #
