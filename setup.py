9c1fd8da32dfbf3218cb0d1c233b867656b392e2
pzaidins2 <paulzaidins2@gmail.com>
2023-12-01 16:08:41 -0500

    packaging attempt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='IPyHOPPER',
+    version='',
+    packages=[ 'ipyhop' ],
+    url='https://github.com/pzaidins2/IPyHOPPER.git',
+    license='',
+    author='paulz',
+    author_email='pzaidins@umd.edu',
+    description='IPyHOPPER HTN Planner'
+)
