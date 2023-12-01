64827d4112a70c551ea1a1b7e962e71771f5cd45
pzaidins2 <paulzaidins2@gmail.com>
2023-12-01 17:13:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='IPyHOPPER',
-    version='0',
+    version='0.0.0',
     packages=[ 'ipyhop' ],
     url='https://github.com/pzaidins2/IPyHOPPER.git',
     license='',
