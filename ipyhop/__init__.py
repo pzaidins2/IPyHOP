fcf155724a2cf4c6824a0d5c86b52c4c1196d172
pzaidins2 <paulzaidins2@gmail.com>
2023-12-01 17:06:29 -0500

    Update __init__.py

--- ipyhop/__init__.py
+++ ipyhop/__init__.py
@@ -1,13 +1,13 @@
 """
 Project:
+    IPyHOPPER - Extension of IPyHOP with Plan Repair
+    Author: Paul Zaidins
+    Copyright (c) 2023, Paul Zaidins
+
+Derived from:
     IPyHOP - Iteration based Hierarchical Ordered Planner
     Author: Yash Bansod
     Copyright (c) 2022, Yash Bansod
-
-Derived from:
-    GTPyhop
-    Author: Dana S. Nau, July 22, 2021
-    Copyright (c) 2021, University of Maryland
 """
 
 from ipyhop.mc_executor import MonteCarloExecutor
@@ -20,6 +20,6 @@ from ipyhop.plotter import planar_plot
 # from ipyhop.failure_handler import post_failure_tasks
 
 """
-Author(s): Yash Bansod
-Repository: https://github.com/YashBansod/IPyHOP
+Author(s): Paul Zaidins
+Repository: https://github.com/pzaidins2/IPyHOPPER.git
 """
